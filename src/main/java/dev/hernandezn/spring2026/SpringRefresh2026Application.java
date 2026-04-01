de1062d33b98fb7200e99fdf34617ae87f56c723
hernandezn <hernandezn@live.com>
2026-04-01 13:29:46 -0400

    Added Spring Boot Starter Files

---
+++ src/main/java/dev/hernandezn/spring2026/SpringRefresh2026Application.java
@@ -0,0 +1,13 @@
+package dev.hernandezn.spring2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRefresh2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRefresh2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/hernandezn/spring2026/SpringRefresh2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hernandezn.spring2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRefresh2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
