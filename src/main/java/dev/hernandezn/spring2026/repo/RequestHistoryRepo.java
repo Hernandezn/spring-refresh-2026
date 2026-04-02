3e3c10dff51ca1782f33c3585894240b57a2d39c
hernandezn <hernandezn@live.com>
2026-04-02 13:48:10 -0400

    Added Repository stereotype to RequestHistoryRepo

--- src/main/java/dev/hernandezn/spring2026/repo/RequestHistoryRepo.java
+++ src/main/java/dev/hernandezn/spring2026/repo/RequestHistoryRepo.java
@@ -1,9 +1,11 @@
 package dev.hernandezn.spring2026.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import dev.hernandezn.spring2026.entity.RequestHistory;
 
+@Repository
 public interface RequestHistoryRepo extends JpaRepository<RequestHistory, Long> {
 
 }
